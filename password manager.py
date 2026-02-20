02afbb2c322cf80509fb248be5241b6c789b382f
necropaulis <actuallycomputers@gmail.com>
2026-02-19 19:06:24 -0500

    password manager - not official

---
+++ password manager.py
@@ -0,0 +1,49 @@
+#a = apphend
+#w = write
+#r = read
+from cryptography.fernet import Fernet
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+master_pwd = input("What is the master password? ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            if "|" not in data:
+                continue
+            user, passw = data.split("|", 1)
+            print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
+            
+def add():
+    name = input("Account name: ")
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        encrypted_pwd = fer.encrypt(pwd.encode()).decode()
+        f.write(name + "|" + encrypted_pwd + "\n")
+
+while True:
+    mode  = input("Would you like to add a new password, or view existing? View, Add, Q to quit ")
+    if  mode == "q":
+        break
+    elif mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid Option")
+        continue
